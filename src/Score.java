24a18e8e869fad3566ea6698d6177be51daba398
Shuriky <hdoo0027@student.monash.edu>
2025-04-03 06:23:01 +1100

    init codes

---
+++ src/Score.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+
+public class Score extends Rectangle {
+    Score () {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+}
