b26cf8e3fe53bdc0fb2a1777b8b0f70d5aced253
Shuriky <hdoo0027@student.monash.edu>
2025-04-03 17:53:18 +1100

    Add prototype

--- src/Pong.java
+++ src/Pong.java
@@ -3,6 +3,20 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
+/**
+ * Steps on how to create a Java executable on IntelliJ
+ * Go to File > Project Structure > Artifacts
+ * Select the "+" icon > JAR > From modules with dependencies
+ * Select the main class (Pong.java)
+ * Select "Extract to the target JAR" for the library
+ * Click "OK"
+ * Click "Apply" and "OK"
+ * Go to Build > Build Artifacts > Build
+ * The JAR file will be created in the out/artifacts directory
+ *
+ * @author Hoang Minh Do - 33746842
+ */
+
 public class Pong {
     public static void main(String[] args) {
 
